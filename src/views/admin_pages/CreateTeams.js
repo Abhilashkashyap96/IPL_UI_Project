556cc59458b99af46cb7676f582b59af138f2fb3
Abhilashkashyap96 <abhilashkashyap.123456@gmail.com>
2022-04-16 11:50:34 +0530

    Updates on the admin pages

--- src/views/admin_pages/CreateTeams.js
+++ src/views/admin_pages/CreateTeams.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { AdminAppHeader, AdminSidebar } from '../../Components/index'
+import { CCard,CCardBody} from '@coreui/react'
+import {CButton, CFormLabel,CInputGroup,CFormInput,CFormCheck } from '@coreui/react'
 
 const CreateTeams = () => {
   return (
@@ -7,10 +9,34 @@ const CreateTeams = () => {
     <AdminSidebar />
     <div className="wrapper d-flex flex-column min-vh-100 bg-light">
       <AdminAppHeader />
-      <div className="body px-3">
-      <h1>Create Teams</h1>
+      <div className="d-flex justify-content-center container"> 
+        <CCard style={{ width:'40rem' }}>
+          <CCardBody>
+            <h3>Create Teams</h3>
+            <CFormLabel >Team Name</CFormLabel>
+            <CInputGroup className="mb-3">
+              <CFormInput placeholder="Username"  aria-describedby="basic-addon1"/>
+            </CInputGroup>
+
+            <CFormLabel >Home Ground</CFormLabel>
+            <CInputGroup className="mb-3">
+              <CFormInput placeholder="Username"  aria-describedby="basic-addon1"/>
+            </CInputGroup>
+
+            <CFormLabel >Team Players</CFormLabel>
+            <CInputGroup className="mb-3">
+              <CFormInput placeholder="Username"  aria-describedby="basic-addon1"/>
+            </CInputGroup>
+
+            <CFormLabel >Captain</CFormLabel>
+            <CInputGroup className="mb-3">
+              <CFormInput placeholder="Username"  aria-describedby="basic-addon1"/>
+            </CInputGroup>
+            <CFormCheck id="flexCheckChecked" label="Validate"  />
+            <CButton color="primary" variant="outline" style={{width:'100%'}}>Create New Team</CButton>
+          </CCardBody>
+        </CCard>
       </div>
-      {/* <AppFooter /> */}
     </div>
   </div>
 
--- src/views/admin_pages/ScheduleMatch.js
+++ src/views/admin_pages/ScheduleMatch.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { AdminAppHeader, AdminSidebar } from '../../Components/index'
+import { CCard,CCardBody} from '@coreui/react'
+import {CButton, CFormLabel,CInputGroup,CFormInput,CFormCheck } from '@coreui/react'
 
 const ScheduleMatch = () => {
   return (
@@ -7,10 +9,40 @@ const ScheduleMatch = () => {
     <AdminSidebar />
     <div className="wrapper d-flex flex-column min-vh-100 bg-light">
       <AdminAppHeader />
-      <div className="body px-3">
-      <h1>Schedule Match</h1>
+      <div className="d-flex justify-content-center container">
+        <CCard style={{ width:'30rem' }}>
+          <CCardBody>
+          <h1>Schedule Match</h1>
+
+            <CFormLabel >Team 1</CFormLabel>
+            <CInputGroup className="mb-3">
+              <CFormInput placeholder="Username"  aria-describedby="basic-addon1"/>
+            </CInputGroup>
+
+            <CFormLabel >Team 2</CFormLabel>
+            <CInputGroup className="mb-3">
+              <CFormInput placeholder="Username"  aria-describedby="basic-addon1"/>
+            </CInputGroup>
+            <div>
+              <div className='d-flex flex-lg-column'>
+                <CFormLabel >Date</CFormLabel>
+                <input type="date" id="start" name="date" value="2022-04-18" min="2022-01-01" max="2022-12-31"/>
+              </div>
+              <div className='d-flex flex-lg-column'>
+                <CFormLabel >Time</CFormLabel>
+                <input type="time" id="appt" name="appt" min="09:00" max="18:00" required/>
+              </div>
+            </div>
+
+            <CFormLabel >Result</CFormLabel>
+            <CInputGroup className="mb-3">
+              <CFormInput placeholder="Username"  aria-describedby="basic-addon1"/>
+            </CInputGroup>
+            <CFormCheck id="flexCheckChecked" label="Validate"  />
+            <CButton color="primary" variant="outline" style={{width:'100%'}}>Schedule Match</CButton>
+          </CCardBody>
+        </CCard>
       </div>
-      {/* <AppFooter /> */}
     </div>
   </div>
   )
